dd558ed2dfaf644215b647869b9d8f6a63647be5
Matthew Perrot <Matthew@MacBook-Pro.local>
2023-04-11 15:43:53 +0000

    create petfax app

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,14 +1,2 @@
-# config                    
-from flask import Flask
-
-# factory
-def create_app():
-    app = Flask(__name__)
-
-    # index route
-    @app.route('/')
-    def index(): 
-        return 'Hello, PetFax!'
-
-    # return the app 
-    return app
+from petfax import create_app
+app = create_app()
