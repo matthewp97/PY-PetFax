38c551e2982a0a47de0c30250b00c3e0a24109c9
Matthew Perrot <Matthew@MacBook-Pro.local>
2023-04-11 16:03:58 +0000

    created css

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,2 +1,18 @@
-from petfax import create_app
-app = create_app()
+# config                    
+from flask import Flask
+
+# factory
+def create_app():
+    app = Flask(__name__)
+
+    # index route
+    @app.route('/')
+    def index(): 
+        return 'Hello, PetFax!'
+
+    # register pet blueprint 
+    from . import pet 
+    app.register_blueprint(pet.bp)
+
+    # return the app 
+    return app
